163874c12dae9c449c055d64c1dbad2bdc89ce92
andrewosmondcode <46552254+andrewosmondcode@users.noreply.github.com>
2019-04-18 08:07:25 +0700

    Fixing bugs

---
+++ main/Game.java
@@ -0,0 +1,22 @@
+package com.monopoly.main;
+
+import javax.swing.JFrame;
+
+public class Game{
+	private JFrame frame = null;
+	
+	public Game() {
+		frame = new JFrame("Monopoly");
+	    frame.setSize(1280, 720);
+
+	    frame.add(new GamePanel());
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
+}
